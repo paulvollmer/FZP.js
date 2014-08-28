7e97d41ce6cd8dcafda6fa5b235b4a5dbdfdad82
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-08-28 14:57:52 +0200

    Created test.FZP.XML.js

---
+++ test/test.FZP.XML.js
@@ -0,0 +1,122 @@
+var assert = require('assert');
+var fs = require('fs');
+var FZP = require('../FZP.XML.js');
+
+describe('FZP.XML.js', function() {
+
+  describe('FZPparseXML', function() {
+    // read a simple test fzp...
+    var xmlstring = fs.readFileSync('./test/files/simple.fzp', {encoding: 'utf8'});
+    // ... and parse it
+    var fzp = FZP.FZPparseXML(xmlstring);
+    
+    describe('module', function() {
+      describe('version', function() {
+        it('check the module version', function() {
+          assert.equal(fzp.module.version, '0.0.0');
+        });
+      });
+
+      describe('id', function() {
+        it('check the module id', function() {
+          assert.equal(fzp.module.id, 'simple');
+        });
+      });
+    });
+
+    describe('version', function() {
+      it('check the version', function() {
+        assert.equal(fzp.version, '1');
+      });
+    });
+
+    describe('author', function() {
+      it('check the author', function() {
+        assert.equal(fzp.author, 'FZP.js');
+      });
+    });
+
+    describe('title', function() {
+      it('check the title', function() {
+        assert.equal(fzp.title, 'simple');
+      });
+    });
+
+    describe('description', function() {
+      it('check the description', function() {
+        assert.equal(fzp.description, 'A simple test file');
+      });
+    });
+
+    describe('label', function() {
+      it('check the label', function() {
+        assert.equal(fzp.label, 'test');
+      });
+    });
+
+    describe('date', function() {
+      it('check the date', function() {
+        assert.equal(fzp.date, '2014-08-27');
+      });
+    });
+
+    describe('tags', function() {
+      it('check the tags array', function() {
+        assert.equal(fzp.tags.length, 2);
+        assert.equal(fzp.tags[0], 'simple');
+        assert.equal(fzp.tags[1], 'test');
+      });
+    });
+
+    describe('properties', function() {
+      it('check the properties array', function() {
+        assert.equal(fzp.properties.length, 2);
+        assert.equal(fzp.properties[0].name, 'package');
+        assert.equal(fzp.properties[0].value, 'simple');
+        assert.equal(fzp.properties[1].name, 'family');
+        assert.equal(fzp.properties[1].value, 'test');
+      });
+    });
+
+    // describe('views', function() {
+    //   describe('icon', function() {
+    //     it('check the icon view object', function() {
+    //       assert.equal(fzp.views.icon, '');
+    //     });
+    //   });
+
+    //   describe('breadboard', function() {
+    //     it('check the breadboard view object', function() {
+    //       assert.equal(fzp.views.breadboard, '');
+    //     });
+    //   });
+
+    //   describe('pcb', function() {
+    //     it('check the pcb view object', function() {
+    //       assert.equal(fzp.views.pcb, '');
+    //     });
+    //   });
+
+    //   describe('schematic', function() {
+    //     it('check the schematic view object', function() {
+    //       assert.equal(fzp.views.schematic, '');
+    //     });
+    //   });
+    // });
+
+    // describe('connectors', function() {
+
+    // });
+
+    // describe('buses', function() {
+
+    // });
+
+
+  });
+
+  // describe('FZPstringifyXML', function() {
+
+  // });
+
+});
